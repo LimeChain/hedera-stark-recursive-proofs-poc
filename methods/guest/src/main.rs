c7dea51da43a71131a29abc6d9a35c2acbdf9cac
Maksim Dimitrov <dimitrov.maksim@gmail.com>
2024-07-16 14:39:23 +0300

    Initial commit
    Signed-off-by: Maksim Dimitrov <dimitrov.maksim@gmail.com>

---
+++ methods/guest/src/main.rs
@@ -0,0 +1,42 @@
+#![no_main]
+
+use risc0_zkvm::{guest::env, serde};
+
+use ark_bn254::{Bn254, Fr, G1Projective, G2Projective};
+use ark_ec::{pairing::Pairing, Group};
+use ark_ff::PrimeField;
+use ark_serialize::CanonicalDeserialize;
+use sha2::{Digest, Sha384};
+
+risc0_zkvm::guest::entry!(main);
+fn main() {
+    // Fetch the input from the environment.
+    let start = env::cycle_count();
+
+    let (pubkey_bytes, message_bytes, signature_bytes): (Vec<u8>, Vec<u8>, Vec<u8>) = env::read();
+    let old_pubkey: Option<Vec<u8>> = env::read();
+    let image_id: [u32; 8] = env::read::<[u32; 8]>();
+
+    if let Some(pk) = old_pubkey {
+        println!("old pubkey: {:?}", pk);
+        env::verify(image_id, &serde::to_vec(&pk).unwrap()).unwrap();
+    }
+
+    let pubkey = G2Projective::deserialize_compressed(&pubkey_bytes[..]).unwrap();
+    let message = G1Projective::deserialize_compressed(&message_bytes[..]).unwrap();
+    let signature = G1Projective::deserialize_compressed(&signature_bytes[..]).unwrap();
+
+    let g2_gen: G2Projective = G2Projective::generator();
+
+    // Pair
+    let pairing_1 = Bn254::pairing(signature, g2_gen);
+    let pairing_2 = Bn254::pairing(message, pubkey);
+
+    assert_eq!(pairing_1, pairing_2);
+
+    let diff = env::cycle_count();
+    env::log(&format!("cycle count after BN254 verify: {}", diff - start));
+
+    // Commit pubkey
+    env::commit(&pubkey_bytes);
+}
