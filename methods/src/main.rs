a084c2eec122842c3840895c5094caca9229ba57
Maksim Dimitrov <dimitrov.maksim@gmail.com>
2024-07-23 16:21:56 +0300

    feat: Add demo mode, fix receipt compression, save succinct receipt aswell
    Signed-off-by: Maksim Dimitrov <dimitrov.maksim@gmail.com>

--- methods/src/main.rs
+++ methods/src/main.rs
@@ -1,15 +1,3 @@
-// use risc0_zkvm::{default_prover, ExecutorEnv, ProverOpts, Receipt, VerifierContext};
-
-// use ark_bn254::{Fr, G1Projective, G2Projective};
-// use ark_ec::Group;
-// use ark_ff::PrimeField;
-// use ark_serialize::CanonicalSerialize;
-// use ark_std::UniformRand;
-// use methods::{PROOFS_ELF, PROOFS_ID};
-// use sha2::{Digest, Sha384};
-
-use std::fs::File;
-
 use anyhow::Context;
 use ark_bn254::{Fr, G1Projective, G2Projective};
 use ark_ec::Group;
@@ -24,79 +12,18 @@ use risc0_zkvm::{
     Groth16Receipt, Groth16ReceiptVerifierParameters, InnerReceipt, ProverOpts, Receipt,
     ReceiptClaim, SuccinctReceipt,
 };
-
+use std::{env, fs::File};
 use sha2::{Digest, Sha384};
 
 pub type SignatureInput = (Vec<u8>, Vec<u8>, Vec<u8>);
 
 pub fn main() -> Result<(), anyhow::Error> {
-    let (pk_0, msg_0, sig_0) = generate_inputs(None)?;
 
-    let prover = default_prover();
-    let no_pk: Option<Vec<u8>> = None;
-    let env_0 = ExecutorEnv::builder()
-        .write(&(pk_0.clone(), msg_0, sig_0))
-        .unwrap()
-        .write(&no_pk)
-        .unwrap()
-        .write(&PROOFS_ID)
-        .unwrap()
-        .build()
-        .unwrap();
-
-    let assumption_receipt = prover.prove(env_0, PROOFS_ELF)?.receipt;
-    assumption_receipt.verify(PROOFS_ID)?;
-
-    let _ = save_receipt(&assumption_receipt, "assumption_receipt");
-
-    let composite_inputs: SignatureInput = generate_inputs(Some(pk_0.clone()))?;
-    let env_1 = ExecutorEnv::builder()
-        .add_assumption(assumption_receipt)
-        .write(&composite_inputs)
-        .unwrap()
-        .write(&Some(pk_0))
-        .unwrap()
-        .write(&PROOFS_ID)
-        .unwrap()
-        .build()
-        .unwrap();
-
-    let prove_info = prover.prove(env_1, PROOFS_ELF)?;
-    let composition_receipt = prove_info.receipt;
-
-    save_receipt(&composition_receipt, "composition_receipt")?;
-
-    composition_receipt.verify(PROOFS_ID)?;
-    // Encode the seal with the selector.
-    let succinct_receipt = prover.compress(&ProverOpts::default(), &composition_receipt)?;
-
-    succinct_receipt.verify(PROOFS_ID)?;
-    let journal_bytes = composition_receipt.journal.bytes.clone();
-    let ident_receipt: SuccinctReceipt<ReceiptClaim> =
-        identity_p254(succinct_receipt.inner.succinct()?).unwrap();
-    let seal_bytes = ident_receipt.get_seal_bytes();
-    let seal = stark_to_snark(&seal_bytes)?.to_vec();
-
-    let groth16_receipt = Receipt::new(
-        InnerReceipt::Groth16(Groth16Receipt::new(
-            seal,
-            ident_receipt.claim.clone(),
-            Groth16ReceiptVerifierParameters::default().digest(),
-        )),
-        journal_bytes.clone(),
-    );
-
-    let _ = save_receipt(&groth16_receipt, "groth16_receipt");
-
-    let seal = groth16_receipt.inner.groth16()?.seal.clone();
-    let calldata = vec![Token::Bytes(journal_bytes), Token::Bytes(encode(seal)?)];
-    let output = hex::encode(ethers::abi::encode(&calldata));
-
-    // Forge test FFI calls expect hex encoded bytes sent to stdout
-    print!("{output}");
-    std::io::stdout()
-        .flush()
-        .context("failed to flush stdout buffer")?;
+    if env::var("RUN_DEMO").is_ok() {
+        run_demo()?;
+    } else {
+        run_prover()?;
+    }
 
     Ok(())
 }
@@ -149,4 +76,113 @@ fn save_receipt(receipt: &Receipt, file_name: &str) -> Result<(), anyhow::Error>
     file.write_all(receipt_bytes.as_bytes())?;
 
     Ok(())
+}
+
+fn run_prover() -> Result<(), anyhow::Error> {
+    let (pk_0, msg_0, sig_0) = generate_inputs(None)?;
+
+    let prover = default_prover();
+    let no_pk: Option<Vec<u8>> = None;
+    let env_0 = ExecutorEnv::builder()
+        .write(&(pk_0.clone(), msg_0, sig_0))
+        .unwrap()
+        .write(&no_pk)
+        .unwrap()
+        .write(&PROOFS_ID)
+        .unwrap()
+        .build()
+        .unwrap();
+
+    let assumption_receipt = prover.prove(env_0, PROOFS_ELF)?.receipt;
+    assumption_receipt.verify(PROOFS_ID)?;
+
+    let _ = save_receipt(&assumption_receipt, "assumption_receipt");
+
+    let composite_inputs: SignatureInput = generate_inputs(Some(pk_0.clone()))?;
+    let env_1 = ExecutorEnv::builder()
+        .add_assumption(assumption_receipt)
+        .write(&composite_inputs)
+        .unwrap()
+        .write(&Some(pk_0))
+        .unwrap()
+        .write(&PROOFS_ID)
+        .unwrap()
+        .build()
+        .unwrap();
+
+    let prove_info = prover.prove(env_1, PROOFS_ELF)?;
+    let composition_receipt = prove_info.receipt;
+
+    save_receipt(&composition_receipt, "composition_receipt")?;
+
+    composition_receipt.verify(PROOFS_ID)?;
+    // Encode the seal with the selector.
+    let succinct_receipt = prover.compress(&ProverOpts::succinct(), &composition_receipt)?;
+
+    save_receipt(&succinct_receipt, "succinct_receipt")?;
+
+    succinct_receipt.verify(PROOFS_ID)?;
+    let journal_bytes = composition_receipt.journal.bytes.clone();
+    let ident_receipt: SuccinctReceipt<ReceiptClaim> =
+        identity_p254(succinct_receipt.inner.succinct()?).unwrap();
+    let seal_bytes = ident_receipt.get_seal_bytes();
+    let seal = stark_to_snark(&seal_bytes)?.to_vec();
+
+    let groth16_receipt = Receipt::new(
+        InnerReceipt::Groth16(Groth16Receipt::new(
+            seal.clone(),
+            ident_receipt.claim.clone(),
+            Groth16ReceiptVerifierParameters::default().digest(),
+        )),
+        journal_bytes.clone(),
+    );
+
+    let _ = save_receipt(&groth16_receipt, "groth16_receipt");
+
+    let calldata = vec![Token::Bytes(journal_bytes), Token::Bytes(encode(seal)?)];
+    let output = hex::encode(ethers::abi::encode(&calldata));
+
+    // Forge test FFI calls expect hex encoded bytes sent to stdout
+    print!("{output}");
+    std::io::stdout()
+        .flush()
+        .context("failed to flush stdout buffer")?;
+
+    Ok(())
+}
+
+fn run_demo() -> Result<(), anyhow::Error> {
+    // fetch receipts from the receipts directory
+    let assumption_receipt: Receipt = read_receipt("assumption_receipt")?;
+    let composition_receipt: Receipt = read_receipt("composition_receipt")?;
+    let succinct_receipt: Receipt = read_receipt("succinct_receipt")?;
+    let groth16_receipt: Receipt = read_receipt("groth16_receipt")?;
+
+    assumption_receipt.verify(PROOFS_ID)?;
+    composition_receipt.verify(PROOFS_ID)?;
+    succinct_receipt.verify(PROOFS_ID)?;
+    groth16_receipt.verify(PROOFS_ID)?;
+
+    let journal = groth16_receipt.journal.bytes.clone();
+    let groth16 = groth16_receipt.inner.groth16()?;
+    let seal = groth16.seal.clone();
+    let encoded_seal = encode(seal)?;
+
+    let calldata = vec![Token::Bytes(journal), Token::Bytes(encoded_seal)];
+    let output = hex::encode(ethers::abi::encode(&calldata));
+
+    // Forge test FFI calls expect hex encoded bytes sent to stdout
+    print!("{output}");
+    std::io::stdout()
+        .flush()
+        .context("failed to flush stdout buffer")?;
+
+    Ok(())
+}
+
+fn read_receipt(file_name: &str) -> Result<Receipt, anyhow::Error> {
+    let receipt: Receipt = serde_json::from_str(
+        &std::fs::read_to_string(format!("receipts/{file_name}.json"))?,
+    )?;
+    Ok(receipt)
 }
